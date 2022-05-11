7879e703abbea881f88470cc03ef88df9a908fed
justbraf <dwaynecorey+github@gmail.com>
2022-05-10 21:59:49 -0400

    font awesome

--- client/main.js
+++ client/main.js
@@ -14,30 +14,51 @@ Template.nav.events({
 
 Template.main.events({
   'click .js-saveProfile'() {
+    // grad data from fields
     let pic = $("#profPic").val();
-    let fname = $("#first").val();
+    let fname = $("#firstN").val();
+    let lname = $("#lastN").val();
     let sex = $("#male").prop("checked") ? "male" : "female";
-    // if ($("#male").prop("checked")) {
-    //   sex = "male";
-    // }
-    // else {
-    //   sex = "female";
-    // }
-    // socialdb.insert({
-    //   "picPath": pic,
-    //   "fname": fname,
-    //   "createdOn": new Date().getTime()
-    // });
-    $("#addModal").modal("hide");
+
+    if (validateAddForm(fname, lname)) {
+      socialdb.insert({
+        "picPath": pic,
+        "fname": fname,
+        "createdOn": new Date().getTime()
+      });
+      $("#addModal").modal("hide");
+    }
   },
   'input #profPic'() {
     let path = $("#profPic").val();
     path = !path ? "unisex-avatar.png" : path;
     $("#displayPic").prop("src", path);
     console.log(path);
+  },
+  'click .js-view'() {
+    let that = this;
+    $("#chkMe").html(that._id);
+    $("#chkMe").html("<h2>" + $("#chkMe").html() + "</h2>profile picture<br>first<br>last<br>age<br>sex<br>description");
+    console.table(that);
+    $("#viewModal").modal("show");
   }
 });
 
+let validateAddForm = (fn, ln) => {
+  let valid = true;
+  $("#firstN").removeClass("errorBox");
+  $("#lastN").removeClass("errorBox");
+
+  if (!fn) {
+    $("#firstN").addClass("errorBox");
+    valid = false;
+  }
+  if (!ln) {
+    $("#lastN").addClass("errorBox");
+    valid = false;
+  }
+  return valid;
+}
 
 Template.profile.helpers({
   profiles() {
